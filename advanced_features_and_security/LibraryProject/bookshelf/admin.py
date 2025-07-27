b1b5c3747b5f5549f67b4130f9ba092a2738fcee
Iroka Chinedu <irokachinedu7@gmail.com>
2025-07-27 18:33:48 +0100

    added more models

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,7 +3,7 @@ from .models import Book
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from django.utils.translation import gettext_lazy as _
-# Register your models here.
+             
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
