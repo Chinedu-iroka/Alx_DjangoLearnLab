c9022ab0a86d843e7a874a2b451c8835587ebc07
Iroka Chinedu <irokachinedu7@gmail.com>
2025-07-27 19:35:14 +0100

    forms

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,9 @@
 from django import forms
-
+from .models import Book 
 class SearchForm(forms.Form):
     title = forms.CharField(max_length=100)
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book  
+        fields = ['title', 'author', 'description']
