18ccdc86a288d39c21a583f5b2f78026c409410f
Iroka Chinedu <irokachinedu7@gmail.com>
2025-08-07 10:04:07 +0100

    still building auth

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Author
