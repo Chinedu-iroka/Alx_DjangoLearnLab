608195f0d2bd764f9a2c511e6aa6c3dac5e8b8da
Iroka Chinedu <irokachinedu7@gmail.com>
2025-08-11 23:18:49 +0100

    first commit

---
+++ django_blog/blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Post
+
+def home(request):
+    posts = Post.objects.all().order_by('-published_date')  # latest first
+    return render(request, 'blog/home.html', {'posts': posts})
